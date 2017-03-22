2cb3ecd7e411995e42405aa27ba427b640e64078
郝进 <haojin@haojindeMacBook-Pro.local>
2017-03-22 23:34:18 +0800

    ios

--- ACM-Template/Other/header.cpp
+++ ACM-Template/Other/header.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <cstdio>
 #include <algorithm>
-#include <vector>
-#include <queue>
-#include <cstring>
-#include <string>
-#include <map>
-#include <set>
-#include <deque>
 #include <climits>
 #include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
 #define ll long long
 #define endl "\n"
 #define DE cout << "------" << endl
@@ -19,8 +19,8 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = 0x3f3f3f3f;
 
-int main()
-{
+int main() {
+    ios::sync_with_stdio(false);
     DE;
     return 0;
 }
