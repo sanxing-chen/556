b74bed5d9a304484ce2ad431399b26db8c51d573
stayinloves <mestayinloves@gmail.com>
2017-03-23 14:41:33 +0800

    update

--- 556-Training/17-03-20/A.cpp
+++ 556-Training/17-03-20/A.cpp
@@ -21,19 +21,20 @@
 using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = 0x3f3f3f3f;
+const ll inf = 1ll << 61;
 using namespace std;
 const int NV = 100005;
-int a[NV], cnt = 1, n;
+ll a[NV], cnt = 1;
 struct node {
     ll oo, ee, oe, eo;
 };
 node sum[NV << 2];
-node fuck(node a, node b) {
+node fuck(node &a, node &b) {
     node c;
-    c.oo = max(a.oo + b.eo, a.oe + b.oo);
-    c.ee = max(a.eo + b.ee, a.ee + b.oe);
-    c.oe = max(a.oo + b.ee, a.oe + b.oe);
-    c.eo = max(a.eo + b.eo, a.ee + b.oo);
+    c.oo = max(max(a.oo + b.eo, a.oe + b.oo), max(a.oo, b.oo));
+    c.ee = max(max(a.eo + b.ee, a.ee + b.oe), max(a.ee, b.ee));
+    c.oe = max(max(a.oo + b.ee, a.oe + b.oe), max(a.oe, b.oe));
+    c.eo = max(max(a.eo + b.eo, a.ee + b.oo), max(a.eo, b.eo));
     return c;
 }
 void PushUp(int rt) {
@@ -42,12 +43,10 @@ void PushUp(int rt) {
 void build(int l, int r, int rt = 1) {
     if (l == r) {
         node &no = sum[rt];
-        if (a[cnt] & 1) {
-            no.oo = 1;
-            no.oe = no.eo = no.ee = 0;
+        if (l & 1) {
+            no.oo = a[cnt];
         } else {
-            no.ee = 1;
-            no.oo = no.eo = no.oe = 0;
+            no.ee = a[cnt];
         }
         cnt++;
         return;
@@ -61,9 +60,39 @@ node query(int L, int R, int l, int r, int rt = 1) {
     if (L <= l && r <= R) return sum[rt];
     int m = (l + r) >> 1;
     node result_1, result_2;
-    if (L <= m) result_1 = query(L, R, lson);
-    if (m < R) result_2 = query(L, R, rson);
-    return fuck(result_1, result_2);
+    bool flag1 = true, flag2 = true;
+    if (L <= m) {
+        result_1 = query(L, R, lson);
+        flag1 = false;
+    }
+    if (m < R) {
+        result_2 = query(L, R, rson);
+        flag2 = false;
+    }
+    if (flag2) return result_1;
+    if (flag1)
+        return result_2;
+    else
+        return fuck(result_1, result_2);
+}
+void update(int pos, ll val, int l, int r, int rt = 1) {
+    if (l == r) {
+        node &no = sum[rt];
+        if (l & 1) {
+            no.oo = val;
+            no.eo = no.ee = no.oe = -inf;
+        } else {
+            no.ee = val;
+            no.eo = no.oo = no.oe = -inf;
+        }
+        return;
+    }
+    int m = (l + r) >> 1;
+    if (pos <= m)
+        update(pos, val, lson);
+    else
+        update(pos, val, rson);
+    PushUp(rt);
 }
 inline int readT() {
     char c;
@@ -78,13 +107,31 @@ inline int readT() {
     return flag ? -ret : ret;
 }
 int main() {
+    int t, n, m;
     cin >> t;
     while (t--) {
+        cnt = 1;
         n = readT();
         m = readT();
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             a[i] = readT();
         }
-        build(0, n - 1)
+        for (int i = 1; i <= (n << 2); i++) {
+            sum[i].oo = sum[i].oe = sum[i].ee = sum[i].eo = -inf;
+        }
+        build(1, n);
+        int op, x;
+        ll y;
+        while (m--) {
+            op = readT();
+            x = readT();
+            y = readT();
+            if (op) {
+                update(x, y, 1, n);
+            } else {
+                node temp = query(x, y, 1, n);
+                cout << max(temp.ee, max(temp.eo, max(temp.oe, temp.oo))) << endl;
+            }
+        }
     }
 }
