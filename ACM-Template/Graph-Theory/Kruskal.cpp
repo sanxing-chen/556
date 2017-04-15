b67be07ba2dbf579fc51599c9b206c39a4855ec5
stayinloves <mestayinloves@gmail.com>
2017-04-15 10:55:33 +0800

    add SMST

--- ACM-Template/Graph-Theory/Kruskal.cpp
+++ ACM-Template/Graph-Theory/Kruskal.cpp
@@ -1,3 +1,4 @@
+/* Kruskal O(elog(e)) */
 const int N = ; //最大点数
 const int M = ; //最大边数
 struct edge {
@@ -33,3 +34,33 @@ struct Kruskal {
         return res;
     }
 } K;
+
+/* 次小生成树 O(n2) */
+bool vis[N];
+int mcos[N][N], nn, mm;
+void dfs(int pos) {
+    for (int i = 0; i < v[pos].size(); i++) {
+        int temp = v[pos][i].v;
+        if (!vis[temp]) {
+            for (int j = 0; j < nn; j++) {
+                if (vis[j]) mcos[temp][j] = mcos[j][temp] = max(mcos[j][pos], v[pos][i].cost);
+            }
+            vis[temp] = true;
+            dfs(temp);
+        }
+    }
+}
+/* 返回增大的值 */
+int next_MST() {
+    int ans = INF;
+    mems(mcos, 0);
+    mems(vis, false);
+    dfs(0);
+    for (int i = 0; i < mm; i++) {
+        edge &temp = K.es[i];
+        /* 边交换：用一条没用过的边替换最小瓶颈路上的最长边 */
+        if (!use[i]) ans = min(ans, temp.cost - mcos[temp.u][temp.v]);
+        // if (!use[i] && temp.cost == mcos[temp.u][temp.v]) return true;
+    }
+    return ans;
+}
