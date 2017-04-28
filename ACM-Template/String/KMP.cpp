fe022759d32d43287330b43e69847bdcdd63f5a2
stayinloves <mestayinloves@gmail.com>
2017-04-28 17:47:56 +0800

    update KMP

--- ACM-Template/String/KMP.cpp
+++ ACM-Template/String/KMP.cpp
@@ -1,15 +1,16 @@
 int f[];
-int KMP(string s, string t) {
-    int m = s.size(), cnt = 0;
+// text and pattern
+int KMP(string t, string p) {
+    int m = p.size(), cnt = 0;
     f[0] = f[1] = 0;
     for (int i = 1; i < m; ++i) {
         int j = f[i];
-        while (j && s[i] != s[j]) j = f[j];
-        f[i + 1] = s[i] == s[j] ? j + 1 : 0;
+        while (j && p[i] != p[j]) j = f[j];
+        f[i + 1] = p[i] == p[j] ? j + 1 : 0;
     }
     for (int i = 0, j = 0; t[i]; ++i) {
-        while (j && t[i] != s[j]) j = f[j];
-        j += (t[i] == s[j]);
+        while (j && t[i] != p[j]) j = f[j];
+        j += (t[i] == p[j]);
         // if (j == m) printf("%d\n", i - m + 1);
         if (j == m) cnt++;
     }
