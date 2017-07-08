436b9c52faae5e2957c61d75b17cb9ba4fd8eabf
stay <mestayinloves@gmail.com>
2017-07-08 11:41:24 +0800

    rename and modify RMQ's st algorithm

--- ACM-Template/dynamic_programing/sparse_table.cpp
+++ ACM-Template/dynamic_programing/sparse_table.cpp
@@ -1,9 +1,10 @@
+// RMQ 问题的 ST 算法，建表复杂度 O(nlogn)，查询复杂度 O(1)
 const int mx = 100000 + 10; //数组最大长度
-ll n, a[mx], k;             //数组长度，数组内容
+int n;                      //数组长度
+ll a[mx];                   //数组内容
 
-int st[mx][30];  // DP数组 维护最小
-int st2[mx][30]; // DP数组 维护最大
-void initRMQ1() {
+int st[mx][30]; // DP数组 维护最小
+void initRMQ() {
     for (int i = 0; i < n; i++) st[i][0] = a[i];
     for (int j = 1; (1 << j) <= n; j++) //使用位运算加速
         for (int i = 0; i + (1 << j) - 1 < n; i++) st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
@@ -13,6 +14,8 @@ int RMQ(int u, int v) {
     int k = (int) (log(v - u + 1.0) / log(2.0)); //类型转换优先级高，除法整体要加括号
     return min(st[u][k], st[v - (1 << k) + 1][k]);
 }
+
+// 保存最小元素的下标
 void initRMQIndex() {
     for (int i = 0; i < n; i++) st[i][0] = i;
     for (int j = 1; (1 << j) <= n; j++)
