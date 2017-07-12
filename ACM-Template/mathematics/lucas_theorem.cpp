f08f93b9d74c221f9ac8e38e7ccdcffe0b1db28e
stay <mestayinloves@gmail.com>
2017-07-12 22:01:54 +0800

    modify lucas

--- ACM-Template/mathematics/lucas_theorem.cpp
+++ ACM-Template/mathematics/lucas_theorem.cpp
@@ -1,26 +1,25 @@
-#define ll long long
-ll PowMod(ll a, ll b, ll MOD) {
-    LL ret = 1;
+ll PowMod(ll a, ll b, ll m) {
+    ll ret = 1;
     while (b) {
-        if (b & 1) ret = (ret * a) % MOD;
-        a = (a * a) % MOD;
+        if (b & 1) ret = ret * a % m;
+        a = a * a % m;
         b >>= 1;
     }
-    return ret % MOD;
+    return ret % m;
 }
 ll fac[100005];
-ll Get_Fact(LL p) {
+ll GetFact(int p) {
     fac[0] = 1;
     for (int i = 1; i <= p; i++) {
-        fac[i] = (fac[i - 1] * i) % p;
+        fac[i] = fac[i - 1] * i % p;
     }
 }
-ll Lucas(LL n, LL m, LL p) {
-    LL ret = 1;
+ll Lucas(ll n, ll m, ll p) {
+    ll ret = 1;
     while (n && m) {
         ll a = n % p, b = m % p;
         if (a < b) return 0;
-        ret = (ret * fac[a] * PowMod(fac[b] * fac[a - b] % p, p - 2, p)) % p;
+        ret = ret * fac[a] * PowMod(fac[b] * fac[a - b] % p, p - 2, p) % p;
         n /= p;
         m /= p;
     }
