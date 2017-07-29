0e8042fd6b46a7643577377a33e3d66780bf2761
stay <mestayinloves@gmail.com>
2017-07-29 16:04:36 +0800

    modify quick_power.cpp

--- ACM-Template/mathematics/quick_power.cpp
+++ ACM-Template/mathematics/quick_power.cpp
@@ -1,5 +1,5 @@
-long long pow(long long a, long long b, long long p) { // return a^b %p
-    long long t = 1;
+ll qpow(ll a, ll b, ll p) { // return a^b %p
+    ll t = 1;
     for (a %= p; b; b >>= 1LL, a = a * a % p)
         if (b & 1LL) t = t * a % p;
     return t;
