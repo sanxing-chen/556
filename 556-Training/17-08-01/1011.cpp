219ab5f8fa23bf0d9b966bf1a9ac469937b1257a
stay <mestayinloves@gmail.com>
2017-08-03 20:01:27 +0800

    add mutc3-4

---
+++ 556-Training/17-08-01/1011.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+//#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+#define DE cout << "------" << endl
+#define mems(a, b) memset(a, b, sizeof a)
+#define REP(i, a, n) for (int i = a; i < (n); i++)
+#define PER(i, a, n) for (int i = (n) -1; i >= a; i--)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+
+int main() {
+    ios::sync_with_stdio(false);
+    ll n, temp, ans = 0;
+    cin >> n;
+    REP(i, 0, n) {
+        cin >> temp;
+        if (temp <= 35) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
