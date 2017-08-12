d7196f6124201fc76eed7b57ebbb0e25d6a89615
Bocity <haolovej@vip.qq.com>
2017-08-12 10:20:28 +0800

    new

--- ACM-Template/mathematics/Inv_from_one_to_n.cpp
+++ ACM-Template/mathematics/Inv_from_one_to_n.cpp
@@ -4,20 +4,16 @@ int inv[maxn];
 inv[1] = 1;
 for (int i = 2; i < 10000; i++) inv[i] = inv[mod % i] * (mod - mod / i) % mod;
 
-
 //快速一个
-inline long long pow_mod(long long a,long long n)  
-{  
-    long long ans=1;  
-    while(n)  
-    {  
-        if(n&1) ans=ans*a%mod;  
-        a=a*a%mod;  
-        n>>=1;  
-    }  
-    return ans;  
+inline long long pow_mod(long long a, long long n) {
+    long long ans = 1;
+    while (n) {
+        if (n & 1) ans = ans * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return ans;
 }
-long long getinv(long long n)  
-{  
-    return pow_mod(n,mod-2);  
+long long getinv(long long n) {
+    return pow_mod(n, mod - 2);
 }
