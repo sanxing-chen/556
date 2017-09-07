1df5d2bfcc224ad9922cf19bfa8d45aa2116d949
stay <mestayinloves@gmail.com>
2017-09-07 21:11:52 +0800

    update qpow

--- ACM-Template/mathematics/quick_power.cpp
+++ ACM-Template/mathematics/quick_power.cpp
@@ -1,6 +1,22 @@
+// p < 1e9
 ll qpow(ll a, ll b, ll p) { // return a^b %p
     ll t = 1;
     for (a %= p; b; b >>= 1LL, a = a * a % p)
         if (b & 1LL) t = t * a % p;
+    return t;
+}
+
+// p > 1e9
+ll qmul(ll a, ll b, ll p) { // return a*b %p
+    ll t = 0;
+    for (; b; a = a * 2 % p, b >>= 1) {
+        if (b & 1) t = (t + a) % p;
+    }
+    return t;
+}
+ll qpow(ll a, ll b, ll p) { // return a^b %p
+    ll t = 1;
+    for (a %= p; b; b >>= 1, a = qmul(a, a, p))
+        if (b & 1) t = qmul(t, a, p);
     return t;
 }
