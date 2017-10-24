55cb4bb389f4d70d75b51d57f392f9a3f2d70539
stay <mestayinloves@gmail.com>
2017-10-24 17:21:01 +0800

    something wierd

---
+++ ACM-Template/string/string_hash.cpp
@@ -0,0 +1,25 @@
+// 字符串哈希，可求出一个字符串中所有子串哈希值。
+// 175为参考值，实际上大于1的质数基本重叠率都很小。
+#define ull unsigned long long
+string str;
+ull xp[maxn], hash0[maxn];
+
+void init() {
+    xp[0] = 1;
+    for (int i = 1; i < maxn; i++) xp[i] = xp[i - 1] * 175;
+}
+
+ull get_hash(int i, int L) {
+    return hash0[i] - hash0[i + L] * xp[L];
+}
+
+int main() {
+    init();
+    cin >> str;
+    int n = str.size();
+    hash[n] = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        hash0[i] = hash0[i + 1] * 175 + (str[i] - 'a' + 1);
+    }
+    now = get_hash(pos, l);
+}
