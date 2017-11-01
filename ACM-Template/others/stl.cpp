56e6a7f20b36c3f5ec93686fe3471ba5ed6e42a2
stay <mestayinloves@gmail.com>
2017-11-01 16:29:06 +0800

    update unique

--- ACM-Template/others/stl.cpp
+++ ACM-Template/others/stl.cpp
@@ -64,3 +64,10 @@ b.to_string('*');      // **1*1*1*
 b.to_string('O', 'X'); // OOXOXOXO
 b.to_ulong();
 b.to_ullong();
+
+// unique
+std::vector<int> v{1, 2, 3, 1, 2, 3, 3, 4, 5, 4, 5, 6, 7};
+std::sort(v.begin(), v.end()); // 1 1 2 2 3 3 3 4 4 5 5 6 7
+auto last = std::unique(v.begin(), v.end());
+// v now holds {1 2 3 4 5 6 7 x x x x x x}, where 'x' is indeterminate
+v.erase(last, v.end());
